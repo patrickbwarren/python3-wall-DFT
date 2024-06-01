6c3de52f6a1df54314668e1884812161ee020f3b
Patrick B Warren <patrickbwarren@gmail.com>
2024-06-01 13:46:52 +0100

    experimental lle

--- lle_interface.py
+++ lle_interface.py
@@ -42,7 +42,7 @@ from numpy import sqrt, exp
 from numpy import log as ln
 from scipy.optimize import root
 
-parser = argparse.ArgumentParser(description='experimental DPD LLE one-off phase coexistence')
+parser = argparse.ArgumentParser(description='experimental DPD LLE profiles')
 parser.add_argument('-r', '--rho', default=3.0, type=float, help='baseline density, default 3.0')
 parser.add_argument('-a', '--allA', default='25,30,25', help='A11, A12, A22, default 25, 30, 25')
 parser.add_argument('-g', '--guess', default='0.01,0.9', help='initial guess x, default 0.01, 0.9')
@@ -82,14 +82,16 @@ if abs(xx[1]-xx[0]) < args.eps:
 else:
     print('Coexisting states likely found, x1, x2 =', xx[0], xx[1])
 
-for i, x in enumerate(xx):
-    a = π/30*(A11*x**2 + 2*A12*x*(1-x) + A22*(1-x)**2)
-    ρ = (sqrt(1 + 4*a*p0) - 1) / (2*a)
-    ρ1, ρ2 = x*ρ, (1-x)*ρ
-    μ1 = ln(ρ1) + π/15*(A11*ρ1 + A12*ρ2)
-    μ2 = ln(ρ2) + π/15*(A12*ρ1 + A22*ρ2)
-    p = ρ + π/30*(A11*ρ1**2 + 2*A12*ρ1*ρ2 + A22*ρ2**2)
-    print(f'phase {i}:  x, ρ  = \t{x}\t{ρ}')
-    print(f'phase {i}: ρ1, ρ2 = \t{ρ1}\t{ρ2}')
-    print(f'phase {i}: μ1, μ2, p = \t{μ1}\t{μ2}\t{p}')
+if args.verbose:
+    for i, x in enumerate(xx):
+        a = π/30*(A11*x**2 + 2*A12*x*(1-x) + A22*(1-x)**2)
+        ρ = (sqrt(1 + 4*a*p0) - 1) / (2*a)
+        ρ1, ρ2 = x*ρ, (1-x)*ρ
+        μ1 = ln(ρ1) + π/15*(A11*ρ1 + A12*ρ2)
+        μ2 = ln(ρ2) + π/15*(A12*ρ1 + A22*ρ2)
+        p = ρ + π/30*(A11*ρ1**2 + 2*A12*ρ1*ρ2 + A22*ρ2**2)
+        print(f'phase {i}:  x, ρ  = \t{x}\t{ρ}')
+        print(f'phase {i}: ρ1, ρ2 = \t{ρ1}\t{ρ2}')
+        print(f'phase {i}: μ1, μ2, p = \t{μ1}\t{μ2}\t{p}')
+
 
