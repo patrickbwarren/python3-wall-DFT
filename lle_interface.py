643d8d4e36b20a029f0a4fdf3af7e21a163ee762
Patrick B Warren <patrickbwarren@gmail.com>
2024-06-02 11:57:31 +0100

    working

--- lle_interface.py
+++ lle_interface.py
@@ -59,9 +59,11 @@ args = parser.parse_args()
 dz = 1e-3
 
 z = np.linspace(-1.0, 1.0, round(2.0/dz)+1, dtype=float)
+
 kernel = π/12.0*(1-z)**3*(1+3*z)
 kernel[z<0] = np.flip(kernel[z>0])
 πby15 = np.trapz(kernel, dx=dz)
+
 if args.verbose:
     print(f'integrated kernel =\t{πby15}')
     print(f'exact result π/15 =\t{π/15}')
@@ -70,8 +72,9 @@ if args.verbose:
 A11, A12, A22 = eval(args.allA) # returns a tuple
 p0 = ρ0 + 1/2*πby15*A11*ρ0**2
 
-print(f'ρ0, A11, A12, A22 = {ρ0} {A11} {A12} {A22}')
-print(f'pressure fixed at p0 = {p0}')
+if args.verbose:
+    print(f'ρ0, A11, A12, A22 = {ρ0} {A11} {A12} {A22}')
+    print(f'pressure fixed at p0 = {p0}')
 
 # Use y = ln(x/(1-x)) which inverts to x = 1/(1+exp(-y)).
 
@@ -84,44 +87,31 @@ def fun(y, p0, A11, A12, A22):
     μ2 = ln(ρ2) + πby15*(A12*ρ1 + A22*ρ2)
     return [μ1[1] - μ1[0], μ2[1] - μ2[0]]
 
-x0 = np.array(eval(f'[{args.guess}]')) # initial guess
+x0 = np.array(eval(f'[{args.guess}]')) # mole fractions in the two phases (initial guess)
 y0 = ln(x0 / (1-x0))
 soln = root(fun, y0, args=(p0, A11, A12, A22))
+yb = soln.x
+xb = 1 / (1 + exp(-yb)) # final mole fractions in the two phases (a 2-vector)
 
 if args.verbose > 1:
     print('Initial guess =', x0)
     print(soln)
 
-x = 1 / (1 + exp(-soln.x))
+if abs(xb[1]-xb[0]) < args.eps:
+    raise ValueError(f'State points likely coalesced, x = {xb[0]}, {xb[1]}')
 
-if abs(x[1]-x[0]) < args.eps:
-    print('State points likely coalesced,  x1, x2 =', x[0], x[1])
-else:
-    print('Coexisting states likely found, x1, x2 =', x[0], x[1])
+a = 1/2*πby15*(A11*xb**2 + 2*A12*xb*(1-xb) + A22*(1-xb)**2)
+ρb = (sqrt(1 + 4*a*p0) - 1) / (2*a) # a 2-vector containing the total densities
+ρ1b, ρ2b = xb*ρb, (1-xb)*ρb # these are 2-vectors containing the coexisting densities
+μ1 = ln(ρ1b) + πby15*(A11*ρ1b + A12*ρ2b) # 2-vector, should be equal in coexistence
+μ2 = ln(ρ2b) + πby15*(A12*ρ1b + A22*ρ2b) # -- ditto --
+p = ρb + 1/2*πby15*(A11*ρ1b**2 + 2*A12*ρ1b*ρ2b + A22*ρ2b**2) # -- ditto --
 
 if args.verbose:
-    for i, xx in enumerate(x):
-        a = 1/2*πby15*(A11*xx**2 + 2*A12*xx*(1-xx) + A22*(1-xx)**2)
-        ρ = (sqrt(1 + 4*a*p0) - 1) / (2*a)
-        ρ1, ρ2 = xx*ρ, (1-xx)*ρ
-        μ1 = ln(ρ1) + πby15*(A11*ρ1 + A12*ρ2)
-        μ2 = ln(ρ2) + πby15*(A12*ρ1 + A22*ρ2)
-        p = ρ + 1/2*πby15*(A11*ρ1**2 + 2*A12*ρ1*ρ2 + A22*ρ2**2)
-        print(f'phase {i}: x, 1-x, ρ = \t{xx}\t{1-xx}\t{ρ}')
-        print(f'phase {i}: ρ1, ρ2 = \t{ρ1}\t{ρ2}')
-        print(f'phase {i}: μ1, μ2, p = \t{μ1}\t{μ2}\t{p}')
+    for v in ['ρb', 'x', 'ρ1b', 'ρ2b', 'μ1', 'μ2', 'p']:
+        print(f'{v:>3} =', eval(v))
 
-a = 1/2*πby15*(A11*x**2 + 2*A12*x*(1-x) + A22*(1-x)**2)
-ρb = (sqrt(1 + 4*a*p0) - 1) / (2*a) # a 2-vector containing the total densities
-ρ1b, ρ2b = x*ρb, (1-x)*ρb # these are 2-vectors containing the coexisting densities
-μ1 = ln(ρ1b) + πby15*(A11*ρ1b + A12*ρ2b)
-μ2 = ln(ρ2b) + πby15*(A12*ρ1b + A22*ρ2b)
-p = ρb + 1/2*πby15*(A11*ρ1b**2 + 2*A12*ρ1b*ρ2b + A22*ρ2b**2) # should be the same
-
-for v in ['ρb', 'x', 'ρ1b', 'ρ2b', 'μ1', 'μ2', 'p']:
-    print(f'{v:>3} =', eval(v))
-
-# algorithm for computing interfacial profiles
+μ1b, μ2b = [np.mean(μ) for μ in [μ1, μ2]] # consensus 'bulk' values
 
 # create an array z in [-zmax, zmax] and a computational domain
 # [-zmax+1, zmax-1] within which the convolution operation is valid.
@@ -133,36 +123,38 @@ lh_bulk = (z < -Lz)
 rh_bulk = (z > Lz)
 domain = ~lh_bulk & ~rh_bulk
 
-ρ1 = np.zeros_like(z)
-ρ1[z<0] = ρ1b[0]
-ρ1[z>0] = ρ1b[1]
-ρ1[z==0] = 0.5*(ρ1b[0] + ρ1b[1])
+def initial_density_profile(ρb):
+    ρ = np.zeros_like(z)
+    ρ[z<0] = ρb[0]
+    ρ[z>0] = ρb[1]
+    ρ[z==0] = 0.5*(ρb[0] + ρb[1])
+    return ρ
 
-ρ2 = np.zeros_like(z)
-ρ2[z<0] = ρ2b[0]
-ρ2[z>0] = ρ2b[1]
-ρ2[z==0] = 0.5*(ρ2b[0] + ρ2b[1])
-
-μ1b = 0.5*(μ1[0] + μ1[1])
-μ2b = 0.5*(μ2[0] + μ2[1])
+def clamp_density_profile(ρ, ρb):
+    ρ[lh_bulk] = ρb[0]
+    ρ[rh_bulk] = ρb[1]
+    return ρ
 
 α = 0.1
 tol = 1e-10
 max_iters = 10**3
 
+ρ1 = initial_density_profile(ρ1b)
+ρ2 = initial_density_profile(ρ2b)
+
+# Solve the following by Picard iteration :
+# ρ_i(z) = exp[ μ_i - ∑_j ∫ dz' ρ_j(z') U_ij(z-z') ]
+
 for i in range(max_iters):
-    ρ1s = dz * np.convolve(ρ1, kernel, mode='same')
-    ρ2s = dz * np.convolve(ρ2, kernel, mode='same')
-    ρ1_new = exp(μ1b - A11*ρ1s - A12*ρ2s)
-    ρ2_new = exp(μ2b - A12*ρ1s - A22*ρ2s)
-    ρ1_new[lh_bulk] = ρ1b[0]
-    ρ1_new[rh_bulk] = ρ1b[1]
-    ρ2_new[lh_bulk] = ρ2b[0]
-    ρ2_new[rh_bulk] = ρ2b[1]
+    ρ1_kern = dz * np.convolve(ρ1, kernel, mode='same')
+    ρ2_kern = dz * np.convolve(ρ2, kernel, mode='same')
+    ρ1_new = clamp_density_profile(exp(μ1b - A11*ρ1_kern - A12*ρ2_kern), ρ1b)
+    ρ2_new = clamp_density_profile(exp(μ2b - A12*ρ1_kern - A22*ρ2_kern), ρ2b)
     ρ1 = (1-α)*ρ1 + α*ρ1_new
     ρ2 = (1-α)*ρ2 + α*ρ2_new
-    int_abs_Δρ = np.trapz(np.abs(ρ1_new-ρ1), dx=dz) + np.trapz(np.abs(ρ2_new-ρ2), dx=dz)
-    if int_abs_Δρ < tol: # early escape if converged
+    int_abs_Δρ1 = np.trapz(np.abs(ρ1_new-ρ1), dx=dz)
+    int_abs_Δρ2 = np.trapz(np.abs(ρ2_new-ρ2), dx=dz)
+    if int_abs_Δρ1 + int_abs_Δρ2 < tol: # early escape if converged
         break
 
 import matplotlib.pyplot as plt
@@ -173,10 +165,3 @@ plt.plot(z[region], ρ1[region])
 plt.plot(z[region], ρ2[region])
 
 plt.show()
-
-# solve the coexistence problem as above and capture chemical potentials
-# initial guess to density profiles could be sharp step function
-
-# Picard iterate the following 
-# rho_i(z) = exp[ - mu_i - sum_j int dz' rho_j(z') U_ij(z-z') ]
-
